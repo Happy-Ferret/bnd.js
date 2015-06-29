78e3db5e3ddc15b7d14a28821127460031600774
Staś Małolepszy <stas@mozilla.com>
2015-06-29 05:19:03 +0200

    Mapping descriptors

--- bound.js
+++ bound.js
@@ -6,25 +6,30 @@
   const forEach = Array.prototype.forEach;
 
   function Bound(model, mapping, events) {
-    function getProperty(prop) {
-      return model[prop];
-    }
-
-    function setProperty(prop, val) {
-      reflectChange(prop, model[prop] = val);
-    }
+    const selectors = Object.create(null);
 
     function reflectChange(prop, val) {
       forEach.call(
-        document.querySelectorAll(mapping[prop]),
+        document.querySelectorAll(selectors[prop]),
         elem => elem.innerHTML = val);
     }
 
     function proxyProperty(prop) {
-      reflectChange(prop, model[prop]);
-      const get = () => getProperty(prop);
-      const set = val => setProperty(prop, val);
+      const descriptor = mapping[prop];
+      selectors[prop] = descriptor.selector ?
+        descriptor.selector : descriptor;
+
+      const get = descriptor.getter ?
+        () => descriptor.getter.call(model) :
+        () => model[prop];
+      const set = descriptor.setter ?
+        val => reflectChange(
+          prop, descriptor.setter.call(model, val)) :
+        val => reflectChange(
+          prop, model[prop] = val);
       Object.defineProperty(this, prop, {get, set});
+
+      reflectChange(prop, get());
     }
 
     Object.keys(mapping).forEach(proxyProperty.bind(this));
--- index.js
+++ index.js
@@ -5,12 +5,18 @@
 
   window.todo = {
     title: 'Bound.js',
-    desc: 'Create the world\'s tiniest two-way binding library'
+    desc: 'Create the world\'s tiniest two-way binding library',
+    //getDesc: function() { return this.desc; },
+    setDesc: function(val) { return this.desc = val; },
   };
 
   window.bound = new Bound(window.todo, {
     title: 'h1',
-    desc: 'p'
+    desc: {
+      selector: 'p',
+      //getter: todo.getDesc,
+      setter: todo.setDesc,
+    }
   }, {
     'click h1': evt => alert(evt.target),
     'change input': evt => this.title = evt.value
