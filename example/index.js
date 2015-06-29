402ad7f6a6209205ca7c211eea8ba53ffb3bf258
Staś Małolepszy <stas@mozilla.com>
2015-06-29 10:14:47 +0200

    Change example

--- example/index.js
+++ example/index.js
@@ -3,15 +3,15 @@
 
   /* jshint esnext:true, -W040, -W093 */
 
-  window.todo = {
+  window.hello = {
     title: 'bnd.js',
-    _desc: 'Create the world\'s tiniest two-way binding library.',
-    owner: 'Staś',
+    _desc: 'A tiny two-way binding library.',
+    owner: 'Staś Małolepszy',
     getDesc: function() { return '<em>' + this._desc + '</em>'; },
     setDesc: function(val) { return this._desc = val; },
   };
 
-  window.bound = new Bnd(window.todo, {
+  window.bound = new Bnd(window.hello, {
     title: {
       'h1': 'textContent, title',
       'input': 'placeholder'
@@ -21,8 +21,8 @@
       sel: {
         'body p': 'innerHTML'
       },
-      get: todo.getDesc,
-      set: todo.setDesc,
+      get: hello.getDesc,
+      set: hello.setDesc,
     }
   }, {
     'click h1': evt => alert(evt.target.textContent),
