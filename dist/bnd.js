57dd4ababc7a30ad7472c3be18abeef39d9a246b
Staś Małolepszy <stas@mozilla.com>
2015-06-29 09:15:55 +0200

    gulpfile

---
+++ dist/bnd.js
@@ -0,0 +1,87 @@
+'use strict';
+
+function _toArray(arr) { return Array.isArray(arr) ? arr : Array.from(arr); }
+
+function Bnd(model, mapping, events) {
+  'use strict';
+
+  /* jshint esnext:true, -W040 */
+
+  var bndSelectors = Object.create(null);
+
+  function forEachMatching(sel, fn) {
+    Array.prototype.forEach.call(document.querySelectorAll(sel), fn);
+  }
+
+  function reflectChange(prop, val) {
+    var sels = bndSelectors[prop];
+    if (typeof sels === 'string') {
+      forEachMatching(sels, function (elem) {
+        return changeAttr(elem, 'textContent', val);
+      });
+    } else {
+      Object.keys(sels).forEach(function (sel) {
+        return forEachMatching(sel, function (elem) {
+          return changeElement(elem, sels[sel], val);
+        });
+      });
+    }
+  }
+
+  function changeElement(elem, attrs, val) {
+    attrs.split(/,\s*/).forEach(function (attr) {
+      return changeAttr(elem, attr, val);
+    });
+  }
+
+  function changeAttr(elem, attr, val) {
+    switch (attr) {
+      case 'textContent':
+      case 'innerHTML':
+        elem[attr] = val;
+        break;
+      default:
+        elem.setAttribute(attr, val);
+    }
+  }
+
+  function proxyProperty(prop) {
+    var descriptor = mapping[prop];
+    bndSelectors[prop] = descriptor.sel || descriptor;
+
+    var get = descriptor.get ? function () {
+      return descriptor.get.call(model);
+    } : function () {
+      return model[prop];
+    };
+    var set = descriptor.set ? function (val) {
+      return reflectChange(prop, descriptor.set.call(model, val));
+    } : function (val) {
+      return reflectChange(prop, model[prop] = val);
+    };
+    Object.defineProperty(this, prop, { get: get, set: set });
+
+    reflectChange(prop, get());
+  }
+
+  function addEventListeners(key) {
+    var _this = this;
+
+    var _key$split = key.split(' ');
+
+    var _key$split2 = _toArray(_key$split);
+
+    var evtName = _key$split2[0];
+
+    var sel = _key$split2.slice(1);
+
+    forEachMatching(sel.join(' '), function (elem) {
+      return elem.addEventListener(evtName, function (evt) {
+        return events[key](evt, _this);
+      });
+    });
+  }
+
+  Object.keys(mapping).forEach(proxyProperty.bind(this));
+  Object.keys(events).forEach(addEventListeners.bind(this));
+}
---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var del = require('del');
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+gulp.task('clean', function(cb) {
+  del(['dist/*'], cb);
+});
+
+gulp.task('transpile', ['clean'], function() {
+  return gulp.src('src/bnd.js')
+    .pipe(babel())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('uglify', ['transpile'], function() {
+  return gulp.src('dist/bnd.js')
+    .pipe(uglify())
+    .pipe(rename('bnd.min.js'))
+    .pipe(gulp.dest('dist'));
+});
+ 
+gulp.task('default', ['uglify']);
